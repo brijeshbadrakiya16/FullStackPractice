4031ebe16aa0070b7ead036480a509ea72594902
brijeshbadrakiya16 <bbadrakiya16@outlook.com>
2026-04-15 12:45:51 +0530

    Day-32 First Commit

---
+++ NodeJs/Day-32/d32.js
@@ -0,0 +1,13 @@
+// Day-32
+
+
+// _______________________________________________________
+
+// #___ S2 E10
+
+// JWT
+// When user hits login with email and password, server will creat a Token and wrape JWT inside it and then user have to store it and send it with all over request, we'll authorize users with that JWT Token.
+
+// learned how to handle cookies with the help of jwt, implemented a auth middle ware that restricts user from accessing apis rather then /signup or /login.
+// learned how to use custom middlewares
+
