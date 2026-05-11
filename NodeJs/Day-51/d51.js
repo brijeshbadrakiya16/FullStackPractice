90a48421e73a6c7b9c16d6fb119db9015aea0b44
brijeshbadrakiya16 <bbadrakiya16@outlook.com>
2026-05-11 18:54:53 +0530

    Day-51 commit

---
+++ NodeJs/Day-51/d51.js
@@ -0,0 +1,36 @@
+// Day-51
+
+// Date: 11/05/2026
+// Task: Created frontend with codex for the snake & ladder backend game, revised some topics from redis, and read again diff between ioredis and node-redis, learned diff between chacha20 and chacha20-poly1305, did some game data entry yudiz-portfolio as Hiten sir asked, started reading the ludo project.
+
+// ______________________________________________________
+
+// completed creating the frontend with help of codex for the snake & ladder backend game
+
+// revised some topics from redis, read difference between ioredis and node-redis
+
+// learned diff between chacha20 and chacha20-poly1305
+// get the code of chacha20 to generate the random number
+
+// _______________________________________________________
+
+// random number generator sites --
+
+// random.org
+// numbergenerator.org
+
+// _______________________________________________________
+
+
+// ++++++++++ DUE +++++++++++
+
+// node-scheduler / cron job
+// redis event - expire ttl
+// pem file
+// redlock
+// socket-io-redis
+// undici
+
+
+// message.js
+// line no 7 and 19 where a single object has same field
