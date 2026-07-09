970e10086f09e1615586b75e83165386fbec03f9
Yudiz Solutions ltd <yudizsolutionsltd@1611.local>
2026-07-09 14:56:31 +0530

    Final

---
+++ FrontEnd/Phaser/Day-94/d94.js
@@ -0,0 +1,11 @@
+// Day-94
+
+// __________________________
+
+// Ending the training.
+
+// Things to learn we want
+// - The end to end deployment process of both frontend and backend, with what the problem might come.
+// - What is the meeting and how it is conducted, what happens in that, and types of meeting.
+// - From getting the project the all process to follow or do from start to end of projects.
+// - And code quality concepts or system design and architecture.
